b077612d1b8f2f56093565f5c0d937193ce44560
Edmund Martin <edmartin101@googlemail.com>
2017-08-13 00:51:45 +0100

    Update amazon_parsers.py

--- scraping_utils/amazon_parsers.py
+++ scraping_utils/amazon_parsers.py
@@ -2,9 +2,38 @@ import asyncio
 
 from lxml import html as htmlparser
 
+
+def parse_product_review(dom):
+    item = dom.cssselect('span.reviewCountTextLinkedHistogram')
+    if item:
+        return item[0].text_content().replace('\n', '').strip()
+    else:
+        return ''
+
+
+def parse_product_title(dom):
+    item = dom.cssselect('h1#title span')
+    if item:
+        return item[0].text_content().replace('\n', '').strip()
+    else:
+        return ''
+
+
+def parse_product_price(dom):
+    item = dom.cssselect('.a-color-price')
+    if item:
+        return item[0].text_content().replace('\n', '').strip()
+    else:
+        return ''
+
 async def amazon_uk_product_parser(response_obj):
-
-    if response_obj.get('html'):
-
-        dom = htmlparser.fromstring(response_obj.get('html'))
+    if response_obj.get('response'):
+        dom = htmlparser.fromstring(response_obj.get('response'))
+        product_review = parse_product_review(dom)
+        product_title = parse_product_title(dom)
+        product_price = parse_product_price(dom)
+        new_data = {'product_review': product_review, 'product_title': product_title, 'product_price': product_price}
+        response_obj.update(new_data)
+        return response_obj
+    return response_obj
 
